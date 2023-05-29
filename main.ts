b21341ef0b9404845dcd86fd98feb64e1e4ff58b
STcraft <18143080+EquinoxTheGryph@users.noreply.github.com>
2023-05-29 14:56:02 +0200

    Create basic npm info retriever

--- main.ts
+++ main.ts
@@ -1,137 +1,105 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { MarkdownRenderChild, Plugin } from "obsidian";
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
-	mySetting: string;
+export default class NpmPreviewPlugin extends Plugin {
+	async onload() {
+		console.log("load");
+		this.registerMarkdownPostProcessor((element, context) => {
+			console.log("register");
+			element.querySelectorAll("code").forEach((element) => {
+				console.log(element);
+				context.addChild(
+					new NpmElement(element, element.innerText.trim())
+				);
+			});
+		});
+	}
+
+	onunload() {}
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+interface NpmPackage {
+	name: string;
+	version: string;
+	description: string;
+	homepage?: string;
+	author: {
+		name: string;
+		email?: string;
+	};
+	deprecated?: string;
+	dependencies?: string[];
+	devDependencies?: string[];
+	keywords?: string[];
+	license: string;
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export class NpmElement extends MarkdownRenderChild {
+	packageName: string;
+
+	constructor(containerEl: HTMLElement, packageName: string) {
+		super(containerEl);
+
+		this.packageName = packageName;
+	}
 
 	async onload() {
-		await this.loadSettings();
+		// Validate Input
+		const validation = this.validateInput(this.packageName);
+		if (validation) {
+			const span = this.containerEl.createSpan({
+				text: validation,
+			});
+			this.containerEl.replaceWith(span);
+			return;
+		}
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
+		// Create element
+		const span = this.containerEl.createSpan({
+			text: `Loading package info for "${this.packageName}"...`,
 		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		this.containerEl.replaceWith(span);
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+		let data: NpmPackage;
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
+		try {
+			data = await this.getPackageInfo(this.packageName);
+		} catch (error) {
+			span.setText(`Error getting package: ${error?.message ?? error}`);
+			return;
+		}
+
+		span.setText(data.description);
+	}
+
+	validateInput(input: string) {
+		if (input.length <= 0) return "Error Validating Input: Input too short";
+		if (input.length > 218) return "Error Validating Input: Input too long";
+		if (!/^[a-zA-Z0-9-][a-zA-Z0-9_-]*$/.test(input))
+			return "Error Validating Input: Invalid characters included";
+		return null;
+	}
+
+	async getPackageInfo(packageName: string): Promise<NpmPackage> {
+		const headers = new Headers();
+
+		headers.set(
+			"Accept",
+			"application/vnd.npm.install-v1+json; q=1.0, application/json; q=0.8, */*"
+		);
+
+		const data = await fetch(
+			`https://registry.npmjs.com/${packageName}/latest`,
+			{
+				headers,
 			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+		);
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+		if (!data.ok) {
+			throw new Error(await data.text());
+		}
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+		return await data.json();
 	}
 }
